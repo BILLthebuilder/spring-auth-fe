a8c9db51014bbaaa2171c5ee7cbe807b587e9f38
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-11 15:40:44 +0300

    add methods to make third party api calls

--- src/app/services/alx-test.service.ts
+++ src/app/services/alx-test.service.ts
@@ -8,8 +8,10 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class RafikiBoraService implements OnInit {
+export class AlxService implements OnInit {
+  private readonly jokesUrl = 'https://official-joke-api.appspot.com/jokes/ten';
 
+  private readonly kitsuManga = 'https://kitsu.io/api/edge/manga';
 
   constructor(private http: HttpClient) {}
   httpOptions = {
@@ -18,6 +20,13 @@ export class RafikiBoraService implements OnInit {
     }),
   };
 
+  getJokes(): Observable<any> {
+    return this.http.get<any>(this.jokesUrl);
+  }
 
+  getKitsuManga(): Observable<any> {
+    return this.http.get<any>(this.kitsuManga);
+  }
+  
   ngOnInit(): void {}
 }
