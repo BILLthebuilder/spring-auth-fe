77dab58c63f6b44d816372bb0c19e74ad8c6a719
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-11 15:38:15 +0300

    refactor home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AlxService } from '../services/alx-test.service';
+import { Joke } from '../services/jokesInterface';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,20 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+busy: boolean = true;
+  constructor(
+    private authService: AuthService,
+    private alxService: AlxService
+  ) {}
+  jokes: Joke[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.alxService
+      .getJokes()
+      .subscribe((data) => {
+        this.jokes = data;
+        this.busy = false;
+      })
+  }
+
 }
